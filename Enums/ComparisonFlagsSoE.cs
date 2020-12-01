5db54ba12189bbdf7f36ac17b329ab088eda37ed
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2020-12-01 23:58:14 +0100

    space characters

--- Enums/ComparisonFlagsSoE.cs
+++ Enums/ComparisonFlagsSoE.cs
@@ -9,21 +9,21 @@ namespace SramComparer.SoE.Enums
 	public enum ComparisonFlagsSoE : uint
 	{
 		[DisplayNameLocalized(nameof(Res.NonGameBuffer), typeof(Res))]
-		NonGameBuffer   = ComparisonFlags.NonGameBuffer,
+		NonGameBuffer = ComparisonFlags.NonGameBuffer,
 		
 		[DisplayNameLocalized(nameof(Res.WholeGameBuffer), typeof(Res))]
 		WholeGameBuffer = ComparisonFlags.WholeGameBuffer,
 
 		[DisplayNameLocalized(nameof(Res.GameChecksum), typeof(Res))]
-		GameChecksum		= 1 << 2,
+		GameChecksum = 1 << 2,
 
 		[DisplayNameLocalized(nameof(Res.AllGameChecksums), typeof(Res))]
-		AllGameChecksums	= 1 << 3 | GameChecksum,
+		AllGameChecksums = 1 << 3 | GameChecksum,
 
 		[DisplayNameLocalized(nameof(Res.Unknown12B), typeof(Res))]
-		Unknown12B	  = 1 << 4,
+		Unknown12B = 1 << 4,
 
 		[DisplayNameLocalized(nameof(Res.AllUnknown12Bs), typeof(Res))]
-		AllUnknown12Bs  = 1 << 5 | Unknown12B,
+		AllUnknown12Bs = 1 << 5 | Unknown12B,
 	}
 }
